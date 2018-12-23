f741459f483c1968c7787bd415c674a1d37bb61c
Uvix Bag <baguvix88@outlook.com>
2018-12-23 13:00:48 +0300

    Add run Tor on button

--- FriendlyTor/Form1.Designer.cs
+++ FriendlyTor/Form1.Designer.cs
@@ -28,13 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button_Start = new System.Windows.Forms.Button();
+            this.button_Stop = new System.Windows.Forms.Button();
+            this.label_Status = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button_Start
+            // 
+            this.button_Start.Location = new System.Drawing.Point(12, 12);
+            this.button_Start.Name = "button_Start";
+            this.button_Start.Size = new System.Drawing.Size(75, 23);
+            this.button_Start.TabIndex = 0;
+            this.button_Start.Text = "Start";
+            this.button_Start.UseVisualStyleBackColor = true;
+            this.button_Start.Click += new System.EventHandler(this.button_Start_Click);
+            // 
+            // button_Stop
+            // 
+            this.button_Stop.Location = new System.Drawing.Point(12, 41);
+            this.button_Stop.Name = "button_Stop";
+            this.button_Stop.Size = new System.Drawing.Size(75, 23);
+            this.button_Stop.TabIndex = 1;
+            this.button_Stop.Text = "Stop";
+            this.button_Stop.UseVisualStyleBackColor = true;
+            this.button_Stop.Click += new System.EventHandler(this.button_Stop_Click);
+            // 
+            // label_Status
+            // 
+            this.label_Status.AutoSize = true;
+            this.label_Status.Location = new System.Drawing.Point(12, 67);
+            this.label_Status.Name = "label_Status";
+            this.label_Status.Size = new System.Drawing.Size(0, 13);
+            this.label_Status.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(647, 401);
+            this.Controls.Add(this.label_Status);
+            this.Controls.Add(this.button_Stop);
+            this.Controls.Add(this.button_Start);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button_Start;
+        private System.Windows.Forms.Button button_Stop;
+        private System.Windows.Forms.Label label_Status;
     }
 }
 
--- FriendlyTor/Form1.cs
+++ FriendlyTor/Form1.cs
@@ -1,20 +1,57 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using FriendlyTorCore;
 
 namespace FriendlyTor
 {
     public partial class Form1 : Form
     {
+        Tor tor = new Tor();
+
         public Form1()
         {
             InitializeComponent();
+            tor.OnTorWork += Tor_OnTorWork;
+        }
+
+        private void Tor_OnTorWork(object sender, EventArgs e)
+        {
+            Action action = () =>
+            {
+                label_Status.Text = "Антиценз работает!";
+            };
+
+            if (InvokeRequired)
+                Invoke(action);
+            else
+                action();
+        }
+
+        private void button_Start_Click(object sender, EventArgs e)
+        {
+            bool result = tor.Start();
+            if (result)
+                label_Status.Text = "Пытаемся запустить Антиценз";
+            else
+                label_Status.Text = "Не удалось запустить Антиценз!";
+        }
+
+        private void button_Stop_Click(object sender, EventArgs e)
+        {
+            bool result = tor.Stop();
+            if (result)
+                label_Status.Text = "Антиценз остановлен";
+            else
+                label_Status.Text = "Ошибка при остановке Антиценз";
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            bool result = tor.Stop();
+            if (result)
+                label_Status.Text = "Антиценз остановлен";
+            else
+                label_Status.Text = "Ошибка при остановке Антиценз";
         }
     }
 }
